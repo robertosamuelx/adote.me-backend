ed1a091c20dd1aff2fbf65402f9a7452a2f56d31
robertosamuelx <roberto-samuel1@hotmail.com>
2020-06-10 22:50:12 -0300

    delete knex and add prisma

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,17 @@ import express from 'express';
 import router from './routes';
 import cors from 'cors';
 const app = express();
+import { PrismaClient } from '@prisma/client';
 
+const prisma = new PrismaClient();
+
+async function select(){
+    const profiles = await prisma.profile.findMany();
+
+    profiles.forEach(profile => {console.log(profile)});
+}
+select()
 app.use(cors());
 app.use(router);
 
-app.listen(3000);
+app.listen(3000,'',()=>{console.log('listening')});
